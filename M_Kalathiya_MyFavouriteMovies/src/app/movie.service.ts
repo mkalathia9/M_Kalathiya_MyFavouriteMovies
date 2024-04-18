55d13661e312e0935e1473a763d6a7363fa2c6cb
mkalathia9 <mkalathia9@gmail.com>
2024-04-18 08:15:28 -0400

    Assignment6

---
+++ M_Kalathiya_MyFavouriteMovies/src/app/movie.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {Content } from './helper-files/content-interface';
+import { CONTENT } from './helper-files/contentDb'; 
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GameService {
+
+  constructor(private messageService: MessageService) { }
+  getContentObs():Observable<Content[]>{
+    const contents = of(CONTENT);
+    this.messageService.add('Content Array Loaded!!');
+    return contents;
+  }
+  getContentById(id:number):Observable<any>{
+    return of(CONTENT.find(item => item.id === id));
+  }
+}
